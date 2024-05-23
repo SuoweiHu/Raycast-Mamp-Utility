786dc72d67d74e63afc2b74e488b629e109fc1f4
Suowei Hu <suowei.h@gmail.com>
2024-05-23 13:39:02 +1000

    Feat: command "manage database" (first draft)

--- src/manage-database.tsx
+++ src/manage-database.tsx
@@ -1,30 +1,68 @@
 import { open_Url_InChrome } from "./utils"
-import { showHUD } from "@raycast/api";
+import { List, getPreferenceValues, render, showHUD } from "@raycast/api";
 import mysql from "mysql";
+import { useEffect, useState } from "react";
+
+
+async function get_databases():Promise<string[]>{
+    // Configure your MySQL connection settings
+    const connection = mysql.createConnection({
+        port       : 8889,
+        host       : "localhost",
+        user       : "root",
+        password   : "root",
+        socketPath : "/Applications/MAMP/tmp/mysql/mysql.sock",
+        database   : "_asbfeo_d10_"
+    });
+
+    // Establish connection and retrive the databases
+    connection.connect((err:any) => {
+        if (err==true) { return console.error('[MYSQL] error connecting: ' + err.stack);                        }
+        else {              /*console.log(  '[MYSQL] connected to database (thred-id=' + connection.threadId);*/}
+    });
+    connection.query("SHOW DATABASES;",(err:any, result) => {
+        if (err==true) { return console.error('[MYSQL] error connecting: ' + err.stack);   }
+        else{
+            /* console.log(  '[MYSQL] result: ' + Object.keys(result)); */
+            for (let i = 0; i < result.length; i++) {
+                const element = result[i];
+                console.log(element);
+            }
+        }
+    });
+    connection.end();
+    // Process the result to retrive the required array of database names
+    return ["asbfeo", "btaa", "calc", "stephanieslater", "superheroesact"];
+}
 
 
 
-// async function getDatabase(
-//     _db_name_: string,
-//     _host_   : string = "localhost",
-//     _user_   : string = "root",
-//     _pass_   : string = "root",
-// ) {
-//     // Configure your MySQL connection settings
-//     const connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'yourUsername',
-//         password: 'yourPassword',
-//         database: 'yourDatabaseName'
-//     });
-
-//     connection.connect((err:any) => {
-//         if (err==true) { return console.error('error connecting: ' + err.stack); }
-//         console.log('connected as id ' + connection.threadId);
-//     });
 
 
-// }
+function ListDatabase(){
+    const [_listDatabases_, set_listDatabases_] = useState<string[]>([]);
+    get_databases(); // .then((data) =>{set_listDatabases_(data)});
+    return (
+        <List>
+            {_listDatabases_.map((item)=>{return <List.Item key={item} title={item}/>})}
+        </List>
+    );
+}
+
+
+
+export default ()=>{
+    const preferences:Preferences=getPreferenceValues();
+    return (<ListDatabase/>);
+}
+
+
+// ███████████████████████████████████████
+// ███████████████████████████████████████
+// ███████████████████████████████████████
+// ███████████████████████████████████████
+// ███████████████████████████████████████
+// ███████████████████████████████████████
 
 // // Runs async. code in a no-view command
 // export default async function Command() {
